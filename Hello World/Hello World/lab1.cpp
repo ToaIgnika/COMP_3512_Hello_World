36cf434bfa9351354fe84e9b9df44026bd31c9b7
Eugene Myroniuk <eugene.my88@gmail.com>
2018-01-14 22:11:48 -0800

    Eugenes solution

--- Hello World/Hello World/lab1.cpp
+++ Hello World/Hello World/lab1.cpp
@@ -1,6 +1,5 @@
 #include "lab1.h"
 
-
 // Greatest Common Denominator
 // PRE: a is a positive integer
 // PRE: b is a positive integer
@@ -8,16 +7,26 @@
 // RETURN: the greatest common denominator of a and b.
 int gcd(const int a, const int b)
 {
-	// Replace this line with your code (have fun!).
-	return 0;
+	int max_denom = 0;
+	for (int i = 1; i <= a && i <= b; i++) {
+		if (a%i == 0 && b%i == 0) {
+			max_denom = i;
+		}
+	}
+	return max_denom;
 }
 
 // Fibonacci
 // PRE: n is a positive int greater than 0
 // POST: n is not changed
-// RETURN: the nth positive integer in the Fibonacci sequence.
+// RETURN: the nth positive integer in the Fibonacci sequence. 1 1 2 3 5 8 13 21 
 long long fibonacci(const int n)
 {
-	// Replace this line with your code (challenge: do this with recursion!).
-	return 0;
+	if (n <= 1) {
+		return 1;
+	}
+	else {
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+	// I am leaving this comments to test the pull request
 }
